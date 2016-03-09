c4b773598899ed8ca0c9719589ffd3f421f7b81b
Jacqui Pickup <jacquipickup@gmail.com>
2016-03-09 00:49:44 +0000

    created ProductPageCrawler

---
+++ models/ProductPageCrawler.php
@@ -0,0 +1,17 @@
+<?php
+namespace models;
+
+class ProductPageCrawler{
+
+	private $total;
+	private $results;
+
+	protected static $path = 'http://hiring-tests.s3-website-eu-west-1.amazonaws.com/2015_Developer_Scrape/5_products.html';
+	
+	public function extractProducts(){
+		return json_encode([
+			'results' => $this->results,
+			'total' => $this->total,
+		]);
+	}
+}
